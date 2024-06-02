5d0dd6fced1e000e6ba79dc2ba5862d062b7f0b7
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2024-06-03 00:41:54 +0800

    AOI v1.0

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from . import Animated_optical_illusions_Zho
+
+NODE_CLASS_MAPPINGS = {**Animated_optical_illusions_Zho.NODE_CLASS_MAPPINGS,}
+NODE_DISPLAY_NAME_MAPPINGS = {**Animated_optical_illusions_Zho.NODE_DISPLAY_NAME_MAPPINGS,}
